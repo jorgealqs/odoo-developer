40b46da9c06eb02c7e6278e8e6cdf804a36593a9
judaqui123 <judaqui123@gmail.com>
2024-08-23 12:01:21 -0500

    [ADD] ODOO-Tutorials/Upload Files, Chapter 1, 2 Finished

---
+++ server-framework-101/addons/estate/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Real Estate",
+    "version": "1.0",
+    "author": "Jorge Alberto Quiroz Sierra",
+    "sequence": -10,
+    'category': 'Odoo/Tutorials',
+    "depends": ["base", "web"],
+    "data": [
+        # "security/ir.model.access.csv",
+        # "views/estate_property_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+    "description": """
+        Estate Management System is an
+        application that allows users to manage properties,
+        property types, property tags, property offers,
+        and inherit data from the base module.
+    """
+}
