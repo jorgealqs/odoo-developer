17cc9689eadb6b03a3c903adb4bf865cf745b0fb
judaqui123 <judaqui123@gmail.com>
2024-08-28 10:58:57 -0500

    [ADD] OWL_Componentys/4_Using_Markup_ToDisplay_HTML

--- server-framework-101/addons/awesome_owl/static/src/card/card.js
+++ server-framework-101/addons/awesome_owl/static/src/card/card.js
@@ -1,16 +1,8 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl"
+import { Component } from "@odoo/owl"
 
 export class Card extends Component {
     static template = "awesome_owl.Card"
-
-    // setup() {
-    //     this.state = useState({ value: 1 })
-    // }
-
-    // increment() {
-    //     this.state.value++
-    // }
 
 }
